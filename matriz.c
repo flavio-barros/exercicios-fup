fdab3a4074f1dd1fd7e504869d7c297c14dfe880
flavio-barros <juniorbarros3@gmail.com>
2020-08-28 15:12:56 -0300

    Novo arquivo de matrizes

--- matriz.c
+++ matriz.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void imprime_maior_linha(int l, int c, int matriz[l][c]){
-  for(int i = 0; i < l; i++){
+void imprimir_matriz(int linhas, int colunas, int matriz[linhas][colunas]){
+  for(int i = 0; i < linhas; i++){
+    for(int j = 0; j < colunas; j++){
+      printf("%d\t", matriz[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+void maior_elemento_linha(int linhas, int colunas, int matriz[linhas][colunas]){
+
+  for(int i = 0; i < linhas; i++){
     int maior = matriz[i][0];
-    for(int j = 1; j < c; j++){
+    for(int j = 1; j < colunas; j++){
       if(matriz[i][j] > maior){
         maior = matriz[i][j];
       }
@@ -12,11 +24,37 @@ void imprime_maior_linha(int l, int c, int matriz[l][c]){
   }
 }
 
+void maior_elemento_coluna(int linhas, int colunas, int matriz[linhas][colunas]){
+
+  for(int i = 0; i < colunas; i++){
+    int maior = matriz[0][i];
+    for(int j = 1; j < linhas; j++){
+      if(matriz[j][i] > maior){
+        maior = matriz[j][i];
+      }
+    }
+    printf("Maior elemento da coluna %d: %d\n", i, maior);
+  }
+}
+
+void somar_matrizes(int l, int c, int matriz1[l][c], int matriz2[l][c], int matriz3[l][c]){
+  for(int i=0; i<l; i++){
+    for(int j=0; j<c; j++){
+      matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+    }
+  }
+}
 
 int main(int argc, char const *argv[]){
+  int matriz1[3][3] = {{2,24,12}, {-2, 65, 21}, {2,4,6}};
+  int matriz2[3][3] = {{3,5,2}, {-1, 7,3}, {4,5,3}};
+  int matriz3[3][3];
 
-  int m1[3][3] = {{5, 3, -2}, {0, -5, 10}, {3, 9, -2}};
-  imprime_maior_linha(3, 3, m1);
-
+  imprimir_matriz(3, 3, matriz1);
+  imprimir_matriz(3, 3, matriz2);
+  somar_matrizes(3, 3, matriz1, matriz2, matriz3);
+  imprimir_matriz(3, 3, matriz3);
+  // maior_elemento_linha(3, 3, matriz1);
+  // maior_elemento_coluna(3, 3, matriz1);
   return 0;
 }
